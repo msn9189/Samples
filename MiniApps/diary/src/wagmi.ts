4ceb2ffba6c108a93309effcff2c47d22231a101
Nazi <mehdizadeh.sn@gmail.com>
2025-10-08 08:39:06 +0000

    frame-wagmi-connector in connectors to work and trigger wallet from Farcaster frame in mobile app

--- MiniApps/diary/src/wagmi.ts
+++ MiniApps/diary/src/wagmi.ts
@@ -1,10 +1,12 @@
 import { createConfig, http } from "wagmi";
 import { base } from "wagmi/chains";
 import { injected } from "wagmi/connectors"; // Supports Metamask and injected wallets
+import frame from "@farcaster/frame-wagmi-connector";
 
 export const config = createConfig({
   chains: [base], // Configure Base network
   connectors: [
+    frame(), // Enable Farcaster in-app wallet connector
     injected(), // Enable MetaMask and other injected wallets
   ],
   transports: {
