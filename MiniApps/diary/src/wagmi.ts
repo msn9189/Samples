c1b37b879d6ef3f9183e675de9c0b4705a913c45
Nazi <mehdizadeh.sn@gmail.com>
2025-10-31 11:29:33 +0000

    walletConnect added to the wagmi.ts

--- MiniApps/diary/src/wagmi.ts
+++ MiniApps/diary/src/wagmi.ts
@@ -1,13 +1,22 @@
 import { createConfig, http } from "wagmi";
 import { base } from "wagmi/chains";
-import { injected } from "wagmi/connectors"; // Supports Metamask and injected wallets
+import { injected, walletConnect } from "wagmi/connectors"; // Supports Metamask and injected wallets
 import frame from "@farcaster/frame-wagmi-connector";
 
 export const config = createConfig({
   chains: [base], // Configure Base network
   connectors: [
     frame(), // Enable Farcaster in-app wallet connector
-    injected(), // Enable MetaMask and other injected wallets
+    injected(),
+    walletConnect({
+      projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
+      metadata: {
+        name: "Diary MiniApp",
+        description: "A MiniApp to write and mint memories on Base network",
+        url: "https://diaryminiapp.vercel.app",
+        icons: ["https://diaryminiapp.vercel.app/DiaryLogo.jpg"],
+      },
+    }), // Enable MetaMask and other injected wallets
   ],
   transports: {
     [base.id]: http("https://mainnet.base.org"), // Base network RPC URL
