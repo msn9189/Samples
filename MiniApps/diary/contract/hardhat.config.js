adfc5ae15e944abc89f4850947b09f175c7fa7df
Nazi <mehdizadeh.sn@gmail.com>
2025-11-01 08:19:16 +0000

    hardhat-verify imported to the hardhat.config.js

--- MiniApps/diary/contract/hardhat.config.js
+++ MiniApps/diary/contract/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-verify");
 
 const { vars } = require("hardhat/config");
 const { base } = require("viem/chains");
